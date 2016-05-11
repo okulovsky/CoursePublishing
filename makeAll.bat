6581ca6481835eb81f71be209c336d60c937ec0e
okulovsky <yuri.okulovsky@gmail.com>
2016-05-11 22:44:02 +0500

    LHPS updated

---
+++ makeAll.bat
@@ -0,0 +1,6 @@
+StructureEditor\bin\Debug\StructureEditor %1
+YoutubeSync\bin\debug\YoutubeSync %1
+Match\bin\Debug\Match %1
+YoutubeCourseSync\bin\Debug\YoutubeCourseSync %1 Preview
+pause
+YoutubeCourseSync\bin\Debug\YoutubeCourseSync %1
