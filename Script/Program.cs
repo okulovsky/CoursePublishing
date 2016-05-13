412bd6917024f9916c2d643d6b28cfa153819c45
okulovsky <yuri.okulovsky@gmail.com>
2016-05-13 10:49:02 +0500

    Format of VideoToCourse is changed to a relational one

--- Script/Program.cs
+++ Script/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoursePublishing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace Script
     {
         static void Main(string[] args)
         {
+            foreach(var e in new[] { "OOP", "LHPS", "Hackerdom" })
+            {
+                var s = Publishing.Courses[e].Load<Structure>();
+                Publishing.SaveCourseStructure(s, e);
+            }
         }
     }
 }
